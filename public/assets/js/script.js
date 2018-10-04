67d202334f6896017aedb94942347acff8f74bb9
ijeli <legaspi.jel@gmail.com>
2018-10-04 03:26:59 -0400

    added search match by date

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -48,3 +48,40 @@ $('#newusersubmitbutton').on('click', function(event) {
     });
 });
 
+$('#matchsubmit').on('click', function(event) {
+    event.preventDefault();
+    var queryURL3 = "https://worldcup.sfg.io/matches";
+    var month = $('#monthofmatch').val().trim();
+    var day = $('#dayofmatch').val().trim();
+    var searchq = "2018-" + month + "-" + day; 
+    console.log(searchq);
+
+    $.ajax({
+        url: queryURL3,
+        method: "GET"
+    }).then(function(response) {
+        var results = response;
+        for (var i = 0; i < results.length; i++) {
+            var date = results[i].datetime;         //2018-06-28T14:00:00Z
+            var sliced = date.slice(0,10);          //2018-06-28
+            var slicedFormat = "YYYY-MM-DD";
+            var convertSliced = moment(sliced, slicedFormat);
+            var currentDate = moment();
+            var diffconvertSliced = moment(currentDate, "YYYY-MM-DD")
+            // .subtract(1, "days");
+
+            tbodymatch = $("#pastmatches");
+            // $('tr').empty();
+            var newRow = $(
+                "<tr><td>" + searchq + "</td></tr><tr><td>" + results[i].home_team.code + " | " + results[i].home_team.goals + "</td><td>"
+                + results[i].away_team.code + " | " + results[i].away_team.goals + "</td><td>" + results[i].winner + "</td></tr>"
+            );
+
+            if ((moment(convertSliced).format("YYYY-MM-DD")) === searchq) {
+                //console.log(this);
+                tbodymatch.append(newRow);
+            }
+        }
+    });
+    $('#tableofMatches').removeClass('hidden');
+});
